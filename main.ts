5a95685134939f5146b353adeb745d274ae8cb12
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-07-21 14:59:56 +0200

    feat: create function setUser which set user turn and display user name

--- main.ts
+++ main.ts
@@ -28,12 +28,36 @@ function onFormSent(e: Event, players: Players): void {
 
   if (players[0].name && players[1].name) {
     clearForm();
+    startGame(players);
   }
 }
 
 function clearForm() {
   const form: HTMLFormElement = document.querySelector(".form");
   form.style.display = "none";
+}
+
+function startGame(players) {
+  let turn = 0;
+  const board = document.querySelector(".board") as HTMLDivElement;
+
+  board.addEventListener("click", (event) => {
+    if (turn % 2 === 0) {
+      turn = 1;
+    } else if (turn % 2 !== 0) {
+      turn = 0;
+    }
+
+    setUser(players, turn);
+  });
+
+  setUser(players, turn);
+}
+
+function setUser(players: Players, turn: number) {
+  const paragraph = document.querySelector(".paragraph");
+
+  paragraph.innerHTML = `Now it's ${players[turn].name}'s turn`;
 }
 
 init();
